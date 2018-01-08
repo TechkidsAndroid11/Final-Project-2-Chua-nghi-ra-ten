585156038785f7436c8ec688ef680be68f1c2e63
DESKTOP-FSA7OK4\Admins <hoangphong143>
2018-01-08 21:41:27 +0700

    LoginFB

--- HotelHunter/app/src/main/java/com/example/admins/hotelhunter/RegisterActivity.java
+++ HotelHunter/app/src/main/java/com/example/admins/hotelhunter/RegisterActivity.java
@@ -2,12 +2,26 @@ package com.example.admins.hotelhunter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+    EditText etEmail;
+    EditText etPassword;
+    EditText etConfirmPass;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        setupUI();
+    }
+
+    private void setupUI() {
+        etEmail=findViewById(R.id.et_emailDK);
+        etPassword=findViewById(R.id.et_passDK);
+        etConfirmPass=findViewById(R.id.et_confirmPassDK);
     }
 }
