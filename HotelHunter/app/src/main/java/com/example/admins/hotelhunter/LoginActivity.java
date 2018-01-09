705d664b869b7f509a5ccd35ea243a3fa816240c
DESKTOP-BRHN9MO\Nguyen Duc Anh <nguyenducanhit97@gmail.com>
2018-01-09 21:16:00 +0700

    login with facebook next

--- HotelHunter/app/src/main/java/com/example/admins/hotelhunter/LoginActivity.java
+++ HotelHunter/app/src/main/java/com/example/admins/hotelhunter/LoginActivity.java
@@ -1,10 +1,13 @@
 package com.example.admins.hotelhunter;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -12,11 +15,20 @@ import com.facebook.login.Login;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
     public static final String TAG = "ABCXYZ";
     LoginButton btLoginFacebook;
     CallbackManager callbackManager;
+    public FirebaseAuth firebaseAuth;
+    public FirebaseAuth.AuthStateListener mAuthListener;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,13 +36,14 @@ public class LoginActivity extends AppCompatActivity {
         setupUI();
     }
 
-    private void setupUI() {
+    public void setupUI() {
         btLoginFacebook = findViewById(R.id.bt_LoginFacebook);
-         callbackManager = CallbackManager.Factory.create();
+        callbackManager = CallbackManager.Factory.create();
         LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 Log.d(TAG, "onSuccess: ");
+                handleFacebookAccessToken(loginResult.getAccessToken());
             }
 
             @Override
@@ -43,6 +56,19 @@ public class LoginActivity extends AppCompatActivity {
                 Log.d(TAG, "onError: " + error.toString());
             }
         });
+        firebaseAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null){
+                    Log.d(TAG, "onAuthStateChanged: " );
+                }
+                else{
+                    Log.d(TAG, "onAuthStateChanged: ");
+                }
+            }
+        };
     }
 
     @Override
@@ -50,5 +76,29 @@ public class LoginActivity extends AppCompatActivity {
         callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
 
+    }
+
+    public void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
     }
 }
