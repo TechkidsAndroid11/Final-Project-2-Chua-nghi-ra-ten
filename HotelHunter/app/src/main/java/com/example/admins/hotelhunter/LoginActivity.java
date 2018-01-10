b96080b077b6aa82f3fe9f57f0c147bcbc0c0a0b
vanphhi96 <vanphi96@gmail.com>
2018-01-10 23:23:44 +0700

    Update login gmail

--- HotelHunter/app/src/main/java/com/example/admins/hotelhunter/LoginActivity.java
+++ HotelHunter/app/src/main/java/com/example/admins/hotelhunter/LoginActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.facebook.AccessToken;
@@ -15,6 +16,13 @@ import com.facebook.login.Login;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
@@ -23,12 +31,15 @@ import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
     public static final String TAG = "ABCXYZ";
     LoginButton btLoginFacebook;
     CallbackManager callbackManager;
     public FirebaseAuth firebaseAuth;
     public FirebaseAuth.AuthStateListener mAuthListener;
+    SignInButton signInButton;
+    GoogleApiClient googleApiClient;
+    private static final int REQ_CODEGOOGLE = 9001;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -69,13 +80,26 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         };
+        signInButton = findViewById(R.id.btn_logingmail);
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this,this).addApi(Auth.GOOGLE_SIGN_IN_API,googleSignInOptions).build();
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
-
+        if(requestCode==REQ_CODEGOOGLE)
+        {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleResult(result);
+        }
     }
 
     public void handleFacebookAccessToken(AccessToken token) {
@@ -100,5 +124,25 @@ public class LoginActivity extends AppCompatActivity {
                         // ...
                     }
                 });
+    }
+    private void handleResult(GoogleSignInResult result) {
+        if(result.isSuccess())
+        {
+            GoogleSignInAccount account = result.getSignInAccount();
+            String name = account.getDisplayName();
+            String email = account.getEmail();
+            String img_url = account.getPhotoUrl().toString();
+            Log.e(TAG, "handleResult: "+name+email+img_url );
+        }
+    }
+    public void signIn()
+    {
+        Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(intent,REQ_CODEGOOGLE);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
