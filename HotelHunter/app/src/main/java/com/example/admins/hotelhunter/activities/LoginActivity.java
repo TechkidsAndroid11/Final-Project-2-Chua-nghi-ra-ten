8c60f3fa2977226139f6aba4d81978d3e5f64db1
vanphhi96 <vanphi96@gmail.com>
2018-01-11 23:57:47 +0700

    fix login gmail

--- HotelHunter/app/src/main/java/com/example/admins/hotelhunter/activities/LoginActivity.java
+++ HotelHunter/app/src/main/java/com/example/admins/hotelhunter/activities/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.admins.hotelhunter.activities;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -30,6 +31,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
     public static final String TAG = "ABCXYZ";
@@ -39,7 +41,8 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     public FirebaseAuth.AuthStateListener mAuthListener;
     SignInButton signInButton;
     GoogleApiClient googleApiClient;
-    private static final int REQ_CODEGOOGLE = 9001;
+    private static final int REQ_CODEGOOGLE = 2;
+    public FirebaseUser firebaseUser;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -73,7 +76,7 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null){
-                    Log.d(TAG, "onAuthStateChanged: " );
+                    Log.d(TAG, "onAuthStateChanged: "+user.toString());
                 }
                 else{
                     Log.d(TAG, "onAuthStateChanged: ");
@@ -81,8 +84,14 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
             }
         };
         signInButton = findViewById(R.id.btn_logingmail);
-        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
-        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this,this).addApi(Auth.GOOGLE_SIGN_IN_API,googleSignInOptions).build();
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build();
+        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+            @Override
+            public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                Toast.makeText(LoginActivity.this, "Somthing went wrong", Toast.LENGTH_SHORT).show();
+            }
+        }).addApi(Auth.GOOGLE_SIGN_IN_API,googleSignInOptions).build();
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -99,7 +108,21 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         {
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
             handleResult(result);
+        }else {
+            Toast.makeText(this, "Auth went wrong", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        firebaseAuth.removeAuthStateListener(mAuthListener);
     }
 
     public void handleFacebookAccessToken(AccessToken token) {
@@ -114,6 +137,7 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithCredential:success");
                             FirebaseUser user = firebaseAuth.getCurrentUser();
+                            Log.e(TAG, "onComplete: "+user );
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInWithCredential:failure", task.getException());
@@ -129,14 +153,26 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         if(result.isSuccess())
         {
             GoogleSignInAccount account = result.getSignInAccount();
-            String name = account.getDisplayName();
-            String email = account.getEmail();
-            String img_url = account.getPhotoUrl().toString();
-            Log.e(TAG, "handleResult: "+name+email+img_url );
+            AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+            firebaseAuth.signInWithCredential(credential)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+
+                                FirebaseUser user = firebaseAuth.getCurrentUser();
+                                Log.d(TAG, "signInWithCredential:success"+user.getEmail().toString()+" "+user.toString());
+
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "signInWithCredential:failure", task.getException());
+
+                            }
+                        }
+                    });
         }
-        else{
-            Log.d(TAG, "handleResult: " + result.getSignInAccount());
-        }
+
+
     }
     public void signIn()
     {
